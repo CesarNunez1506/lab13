2b06475f3a2ba8eaf804bdb5bbe1a1bc998910e3
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2024-11-04 21:58:39 -0500

    chore:AnimatedVisibilityExample

--- app/src/main/java/com/example/lab/MainActivity.kt
+++ app/src/main/java/com/example/lab/MainActivity.kt
@@ -12,6 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab.ui.theme.LabTheme
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AnimatedVisibilityExample()
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -37,7 +48,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun AnimatedVisibilityExample() {
+    var isVisible by remember { mutableStateOf(false) }
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { isVisible = !isVisible }) {
+            Text("Activar Visibilidad")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Blue)
+            )
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
